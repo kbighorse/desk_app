6a098bf2d11a1f9ee5e838c843ce6f058aad6903
Kimball Bighorse <kbighorse@yahoo.com>
2014-09-12 04:56:32 -0700

    Style and labels index view

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 DeskApp::Application.routes.draw do
+  get "labels/index"
+
   resources :filters, only: [:index, :show]
+  resources :labels, only: [:index]
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
@@ -50,7 +53,7 @@ DeskApp::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  root to: 'welcome#index'
+  root to: 'filters#index'
 
   # See how all your routes lay out with "rake routes"
 
